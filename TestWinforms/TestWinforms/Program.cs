b9363c6630ef876a4b96676a310377cdc7303d1d
Ramon Balaguer Altadill <rbalaguer@voxelgroup.net>
2018-02-05 10:47:19 +0100

    Mod to release compilation

--- TestWinforms/TestWinforms/Program.cs
+++ TestWinforms/TestWinforms/Program.cs
@@ -14,6 +14,7 @@ namespace TestWinforms
         [STAThread]
         static void Main()
         {
+            as
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
